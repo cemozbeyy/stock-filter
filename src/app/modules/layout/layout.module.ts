428656c0003b596a02ee9f092ea0fcc34cd66b8a
cemozbey <48527545+cemozbeyy@users.noreply.github.com>
2022-10-30 15:18:12 +0300

    sidenav inprocress

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { TstHeaderComponent, TstMainTableComponent, TstSidenavComponent } from './components';
+import { MatIconModule } from '@angular/material/icon';
 
 import { LayoutComponent } from './layout-component';
 
-
+const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent
+    }
+]
 
 const COMPONENTS = [
     LayoutComponent,
@@ -17,6 +24,8 @@ const COMPONENTS = [
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule.forChild(routes),
+        MatIconModule
     ],
     exports: [],
     schemas: [
