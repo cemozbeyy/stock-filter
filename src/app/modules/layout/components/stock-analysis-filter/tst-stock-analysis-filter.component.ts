19ca2425081d8ead9bb2970163bce0873a49eb6a
cemozbey <48527545+cemozbeyy@users.noreply.github.com>
2022-10-30 22:28:20 +0300

    DATE FORMAT DONE.

--- src/app/modules/layout/components/stock-analysis-filter/tst-stock-analysis-filter.component.ts
+++ src/app/modules/layout/components/stock-analysis-filter/tst-stock-analysis-filter.component.ts
@@ -1,17 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import * as materialMomentAdapter from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
 import { FormControl } from '@angular/forms';
 import { TimeRange } from 'src/app/modules/core/helpers/time-range.model';
+
+
+export const MY_FORMATS = {
+    parse: {
+        dateInput: 'YYYY/DD/MM',
+    },
+    display: {
+        dateInput: 'YYYY/DD/MM',
+        monthYearLabel: 'YYYY MMM',
+        dateA11yLabel: 'LL',
+        monthYearA11yLabel: 'YYYY MMM',
+    },
+};
+
 
 @Component({
     selector: 'tst-filter',
     templateUrl: 'tst-stock-analysis-filter.component.html',
-    styleUrls: ['tst-stock-analysis-filter.component.scss']
+    styleUrls: ['tst-stock-analysis-filter.component.scss'],
+    providers: [
+        // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+        // application's root module. We provide it at the component level here, due to limitations of
+        // our example generation script.
+        {
+            provide: DateAdapter,
+            useClass: materialMomentAdapter.MomentDateAdapter,
+            deps: [MAT_DATE_LOCALE, materialMomentAdapter.MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+        },
+
+        { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+    ]
 })
 
 export class TstStockAnalysisFilterComponent implements OnInit {
-    constructor() { }
+    constructor() {
+
+    }
     selectedTime!: string
-    stockList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+    selectedStock!: string[]
+    selectedDate!: string
+    stockList: string[] = ['IBM', 'AAPL', 'MSFT', 'AMZN', 'GOOG'];
     timeRange: TimeRange[] = [
         { timeName: '15min' },
         { timeName: '30min' },
@@ -23,5 +56,8 @@ export class TstStockAnalysisFilterComponent implements OnInit {
     ngOnInit() {
 
     }
-
+    dateRangeChange(dateRangeStart: HTMLInputElement, dateRangeEnd: HTMLInputElement) {
+        console.log(dateRangeStart.value);
+        console.log(dateRangeEnd.value);
+    }
 }
